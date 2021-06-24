6006132089c3c2c6651608a8f15e52c03924620d
Kevin Funez <kevin.funez315@gmail.com>
2021-06-24 16:35:34 -0700

    wait time in console log
    > Co-Authored-By: Allister <allisterkmoon@gmail.com>
    > Co-Authored-By: John Blalock <simpledimplejohn@gmail.com>

--- src/main.js
+++ src/main.js
@@ -11,35 +11,65 @@ console.log(`
   --
 `);
 
-const game = new Game();
 
-for (; !game.player01.isDead() && !game.enemy01.isDead();) {  // 
-  // Math.floor(Math.random() * (max - min + 1) + min)
 
-  // attack, defend
-  // 1 - 2
+// function doThing () {
+//   return 42
+// }
 
-  let value =  Math.floor(Math.random() * (2 - 1 + 1) + 1);
-  if (value === 1) {
-    game.player01.action = 'attack';
-  } else if (value === 2) {
-    game.player01.action = 'defend';
+// const ididthing = doThing()
+
+async function doTheThing () {
+  const game = new Game();
+
+  for (; !game.player01.isDead() && !game.enemy01.isDead();) {  // 
+    // Math.floor(Math.random() * (max - min + 1) + min)
+
+    // attack, defend
+    // 1 - 2
+    
+    let value =  Math.floor(Math.random() * (2 - 1 + 1) + 1);
+    if (value === 1) {
+      game.player01.action = 'attack';
+    } else if (value === 2) {
+      game.player01.action = 'defend';
+    }
+    let valueTwo = Math.floor(Math.random()* (2 - 1 + 1) + 1);
+    if (valueTwo === 1) {
+      game.enemy01.action = 'attack';
+    } else if (valueTwo === 2) {
+      game.enemy01.action = 'defend';
+    }
+
+    game.fight(
+      {action: game.player01.action},
+      {action: game.enemy01.action},
+    );
+
+    console.log('enemy currentHealth: ',game.enemy01.currentHealth);
+
+    console.log('player currentHealth: ',game.player01.currentHealth);
+
+    await new Promise(function (resolve) {
+      setTimeout(function () {
+        resolve();
+      }, 1000);
+    });
   }
-  let valueTwo = Math.floor(Math.random()* (2 - 1 + 1) + 1);
-  if (valueTwo === 1) {
-    game.enemy01.action = 'attack';
-  } else if (valueTwo === 2) {
-    game.enemy01.action = 'defend';
+
+  if (game.player01.isDead()) {
+    console.log("%cYou Lose!", 'font-size: 40px;');
+  } else {
+    console.log("%cYou Win!", 'font-size: 40px;');
   }
-
-  game.fight(
-    {action: game.player01.action},
-    {action: game.enemy01.action},
-  );
-
-  console.log('enemy maxHealth: ',game.enemy01.maxHealth);
-  console.log('enemy currentHealth: ',game.enemy01.currentHealth);
-
-  console.log('player maxHealth: ',game.player01.maxHealth);
-  console.log('player currentHealth: ',game.player01.currentHealth);
 }
+
+doTheThing();
+
+// console.log(`
+// x x
+//  -
+// `)
+
+// THATS AWESOME!!! 
+// YEA LETS DO IT!
